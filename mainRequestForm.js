3d54b16e3aa4836205d78ebf1f33e3bd4cc7240a
jvits <jscvito@gmail.com>
2018-03-21 09:10:41 +0800

    git

--- mainRequestForm.js
+++ mainRequestForm.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
-    alert($("#eventCode").getValue());
 
-$("#memoEventCode").setValue("Event Code: "+$("#eventCode").getText());
-
-$("#eventCode").setOnchange(function(){
-    $("#memoEventCode").setValue("Event Code: "+$("#eventCode").getText());
-});
-
-
-$("#eventTitle").setOnchange(function(){
-    $("#memoWhat").setValue("What: "+$("#eventTitle").getText()+" "+$("#description").getText());
-});
-
-$("#description").setOnchange(function(){
-    $("#memoWhat").setValue("What: "+$("#eventTitle").getText()+": "+$("#description").getText());
-});
-
-alert('sample');
+    alert('hi im from github');
 
 });
